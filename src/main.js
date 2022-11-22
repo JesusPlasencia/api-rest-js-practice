96e62a23fc9c740c0ac6a49e87f88dc672a4b24a
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-11-22 17:03:51 -0500

    Trending Movies

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-console.log("Hello!");
+const TRENDING_MOVIE_DAY = 'https://api.themoviedb.org/3/trending/movie/day';
+const IMAGE = 'https://image.tmdb.org/t/p/w300';
+
+async function getTrendingMoviesPreview() {
+    const request = await fetch(`${TRENDING_MOVIE_DAY}?api_key=${API_KEY}`);
+    const data = await request.json();
+    const movies = data.results;
+    //
+    const trendingPreviewListContainer = document.querySelector('.trendingPreview-movieList');
+    //
+    movies.forEach(movie => {
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const imageContainer = document.createElement('img');
+        imageContainer.classList.add('movie-img');
+        imageContainer.setAttribute('alt', movie?.title);
+        console.log(IMAGE + movie?.poster_path);
+        imageContainer.setAttribute('src', `${IMAGE + movie?.poster_path}`);
+
+        movieContainer.appendChild(imageContainer);
+        trendingPreviewListContainer.appendChild(movieContainer);
+    });
+}
+
+getTrendingMoviesPreview()
